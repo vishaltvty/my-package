db3e0bdf155777d26267a9f6fb31ceed9dcf1077
vishaltvty <Vishal.Chilka@tivityhealth.com>
2020-12-07 15:17:16 -0700

    Added net project

---
+++ nuget/Applogger/Applogger/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Applogger
+{
+    public class Logger
+    {
+        public static void Log(string text)
+        {
+            Console.WriteLine("Your log: " + text);
+        }
+    }
+}
---
+++ nuget/Applogger/NetApploggerTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using Applogger;
+using System;
+
+namespace NetApploggerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic obj = new Logger();
+
+            obj.Log("this is atest");
+        }
+    }
+}
